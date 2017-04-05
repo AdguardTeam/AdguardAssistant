2ac81007c3e366e10186da13af137738fe603ed8
Suhan <suhandd@gmail.com>
2017-04-05 16:32:39 +0300

    https://github.com/AdguardTeam/AdguardAssistant/issues/3 project layout

--- compiler.meta.js
+++ compiler.meta.js
@@ -40,42 +40,42 @@
 // @exclude *vk.com/widget_comments*
 // @exclude *www.youtube.com/embed/*
 // @exclude *://outlook.live.com*
-// @require utils\css.escape.js
-// @require ioc.js
-// @require log.js
-// @require balalaika.patched.js
-// @require settings.js
-// @require utils\ui-utils.js
-// @require utils\common-utils.js
-// @require utils\punycode.js
-// @require event.js
-// @require selector\diff_match_patch.js
-// @require selector\dom.patched.js
-// @require selector\adguard-selector.js
-// @require adguard-rules-constructor.js
-// @require iframe.js
-// @require gm.js
-// @require slider-widget.js
-// @require wot.js
-// @require _locales\ru.js
-// @require _locales\en.js
-// @require localization.js
-// @require controllers\mainMenuController.js
-// @require controllers\selectorMenuController.js
-// @require controllers\sliderMenuController.js
-// @require controllers\blockPreviewController.js
-// @require controllers\settingsMenuController.js
-// @require button.js
-// @require main.js
-// @resource button.css styles\button.css
-// @resource selector.css styles\selector.css
-// @resource button.html    templates\button.html
-// @resource style.css  styles\style.css
-// @resource mainMenu.html templates\mainMenu.html
-// @resource selectorMenu.html templates\selectorMenu.html
-// @resource sliderMenu.html templates\sliderMenu.html
-// @resource blockPreview.html templates\blockPreview.html
-// @resource settingsMenu.html templates\settingsMenu.html
+// @require src\utils\css.escape.js
+// @require src\ioc.js
+// @require src\log.js
+// @require src\balalaika.patched.js
+// @require src\settings.js
+// @require src\utils\ui-utils.js
+// @require src\utils\common-utils.js
+// @require src\utils\punycode.js
+// @require src\event.js
+// @require src\selector\diff_match_patch.js
+// @require src\selector\dom.patched.js
+// @require src\selector\adguard-selector.js
+// @require src\adguard-rules-constructor.js
+// @require src\iframe.js
+// @require src\gm.js
+// @require src\slider-widget.js
+// @require src\wot.js
+// @require src\_locales\ru.js
+// @require src\_locales\en.js
+// @require src\localization.js
+// @require src\controllers\mainMenuController.js
+// @require src\controllers\selectorMenuController.js
+// @require src\controllers\sliderMenuController.js
+// @require src\controllers\blockPreviewController.js
+// @require src\controllers\settingsMenuController.js
+// @require src\button.js
+// @require src\main.js
+// @resource button.css src\styles\button.css
+// @resource selector.css src\styles\selector.css
+// @resource button.html    src\templates\button.html
+// @resource style.css  src\styles\style.css
+// @resource mainMenu.html src\templates\mainMenu.html
+// @resource selectorMenu.html src\templates\selectorMenu.html
+// @resource sliderMenu.html src\templates\sliderMenu.html
+// @resource blockPreview.html src\templates\blockPreview.html
+// @resource settingsMenu.html src\templates\settingsMenu.html
 // @grant	GM_getValue
 // @grant	GM_setValue
 // @grant   GM_getResourceText

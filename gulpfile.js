65c3968520403296c9258bf7e5895554858a7b68
4-life <pavel@ovchinnikov.us>
2017-07-07 18:40:20 +0300

    Fix tests file for gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -48,8 +48,7 @@ gulp.task('testsToGhPages', () => {
         'test/**',
         'node_modules/mocha/mocha.*',
         'node_modules/chai/chai.js'
-    ], {base:"."})
-        .pipe(gulp.dest(options.outputPath));
+    ]).pipe(gulp.dest(options.outputPath + '/test/'));
 });
 
 

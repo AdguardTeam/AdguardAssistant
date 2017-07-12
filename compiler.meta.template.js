b20708ac5b51b2256e54cd4ca21bb3fe4ff3b61e
4-life <pavel@ovchinnikov.us>
2017-07-12 16:46:21 +0300

    4.1.0

--- compiler.meta.template.js
+++ compiler.meta.template.js
@@ -4,7 +4,7 @@
 // @namespace Adguard
 // @description Provides easy and convenient way to manage filtering right from the browser
 // [DESCRIPTIONS_IN_OTHER_LANGUAGES_PLACEHOLDER]
-// @version 4.0.9
+// @version 4.1.0
 // @downloadURL [DOWNLOAD_URL]
 // @updateURL [UPDATE_URL]
 // @include *

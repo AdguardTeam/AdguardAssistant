d47352ba0a9ad8d81616807ac7a9365105effce3
ovchinnikov <p.ovchinnikov@adguard.com>
2018-03-01 17:35:57 +0300

    Fix copyright task

--- tasks/compile.js
+++ tasks/compile.js
@@ -109,7 +109,14 @@ module.exports = () => {
         content.push('})();');
 
         if (options.embedded) {
-            copyright = fs.readFileSync('./embedded-copyright.txt').toString();
+            let currentDate = {
+                year: (new Date()).getFullYear(),
+                month: ('0' + ((new Date()).getMonth()+1)).slice(-2),
+                day: ('0' + (new Date()).getDate()).slice(-2)
+            };
+
+            copyright = `/*! AdGuard Assistant - v${options.version} - ${currentDate.year}-${currentDate.month}-${currentDate.day}\r\n* https://github.com/AdguardTeam/AdguardAssistant\r\n* Copyright (c) ${currentDate.year}; Licensed LGPL 3.0 */\r\n`;
+
             content.unshift(copyright);
         }
     };

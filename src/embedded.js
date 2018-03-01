10e3f2cc8c7a798bf99a251fdfe64c006318c175
ovchinnikov <p.ovchinnikov@adguard.com>
2018-03-01 15:24:51 +0300

    Upd

--- src/embedded.js
+++ src/embedded.js
@@ -73,7 +73,7 @@ var adguardAssistantMini = function() {
 var mobileReg = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i;
 
 if (mobileReg.test(navigator.userAgent)) {
-    this.adguardAssistant = adguardAssistantMini();
+    this.adguardAssistant = adguardAssistantMini;
 } else {
-    this.adguardAssistant = adguardAssistantExtended();
+    this.adguardAssistant = adguardAssistantExtended;
 }

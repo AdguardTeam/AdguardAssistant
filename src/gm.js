09f56a0c4fc21813d82bca085c619fa964446136
Tim Shenin <t.shenin@adguard.com>
2019-08-22 14:06:15 +0300

    Fix gm api for all builds

--- src/gm.js
+++ src/gm.js
@@ -30,41 +30,41 @@ import protectedApi from './protectedApi';
  * @constructor
  */
 function GM() {
-    if (typeof ADG_addRule === 'undefined') {
-        ADG_addRule = (rule, callback) => {
+    const addRule = typeof ADG_addRule !== 'undefined'
+        ? ADG_addRule
+        : (rule, callback) => {
             alert(`GM_api is not supported. ${rule} rule added`);
             if (callback) callback();
         };
-    }
 
     // TODO: fix calling this one
-    if (typeof ADG_sendAbuse === 'undefined') {
-        ADG_sendAbuse = (url, callback) => {
+    const sendAbuse = typeof ADG_sendAbuse !== 'undefined'
+        ? ADG_sendAbuse
+        : (url, callback) => {
             alert(`GM_api is not supported. ${url} abused`);
             if (callback) callback();
         };
-    }
 
-    if (typeof ADG_temporaryDontBlock === 'undefined') {
-        ADG_temporaryDontBlock = (timeout, callback) => {
+    const temporaryDontBlock = typeof ADG_temporaryDontBlock !== 'undefined'
+        ? ADG_temporaryDontBlock
+        : (timeout, callback) => {
             alert(`GM_api is not supported. Do not block for ${timeout} seconds`);
             if (callback) callback();
         };
-    }
 
-    if (typeof ADG_isFiltered === 'undefined') {
-        ADG_isFiltered = (callback) => {
-            if (callback) callback();
+    const isFiltered = typeof ADG_isFiltered !== 'undefined'
+        ? ADG_isFiltered
+        : (callback) => {
+            if (callback) callback(true);
             return true;
         };
-    }
 
-    if (typeof ADG_changeFilteringState === 'undefined') {
-        ADG_changeFilteringState = (callback) => {
+    const changeFilteringState = typeof ADG_changeFilteringState !== 'undefined'
+        ? ADG_changeFilteringState
+        : (state, callback) => {
             alert('GM_api is not supported. State changed');
             if (callback) callback();
         };
-    }
 
     const getValue = value => new Promise(((resolve) => {
         resolve(GM_getValue(value));
@@ -80,11 +80,11 @@ function GM() {
         setValue,
         GM_getResourceText: GM_getResourceText,
         GM_addStyle: GM_addStyle,
-        ADG_addRule,
-        ADG_temporaryDontBlock,
-        ADG_sendAbuse,
-        ADG_isFiltered,
-        ADG_changeFilteringState,
+        ADG_addRule: addRule,
+        ADG_temporaryDontBlock: temporaryDontBlock,
+        ADG_sendAbuse: sendAbuse,
+        ADG_isFiltered: isFiltered,
+        ADG_changeFilteringState: changeFilteringState,
     };
 }
 // eslint-disable-next-line import/no-mutable-exports

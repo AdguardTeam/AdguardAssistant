8d7b594254c389daedf2925a3df7f35ba9925691
Anton <a.kaprielov@adguard.com>
2021-09-09 16:23:23 +0300

    feature/AG-9984 fix test

--- tests/assistant.test.js
+++ tests/assistant.test.js
@@ -1,12 +1,10 @@
 /* global QUnit */
-import { adguardAssistant } from '../src/index'; // import assistant into tests
+import '../src/index'; // import assistant into tests
 
 const { test, module } = QUnit;
 
 module('Assistant main');
 
 test('Should be true', (assert) => {
-    const assistant = adguardAssistant();
-    assistant.start(null, (...args) => { console.log(args); });
     assert.ok(true, 'assistant didn\'t cause errors on the page');
 });
